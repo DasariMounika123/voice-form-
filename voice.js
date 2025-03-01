4ab0ac731fab18920e657f5083740b7b20593425
DasariMounika123 <2022cse.r523@svck.edu.in>
2025-03-01 21:17:24 +0530

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,9 +1,9 @@
-function startVoiceRecognition() {
+function startVoiceRecognition(inputId) {
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
     recognition.lang = "en-US";
 
     recognition.onresult = (event) => {
-        document.getElementById("username").value = event.results[0][0].transcript;
+        document.getElementById(inputId).value = event.results[0][0].transcript;
     };
 
     recognition.start();
